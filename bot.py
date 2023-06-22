da4c1a0fb8ee371a1e833609bf26c05fe72d9d54
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-06-22 14:07:03 +0800

    Inital mongodb setup

--- bot.py
+++ bot.py
@@ -1,32 +1,57 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from pymongo import MongoClient
+from telegram import Bot
+from telegram.ext import CommandHandler, Updater
 
-# Handler for the /start command
-def start(update: Update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Hello! I am your Telegram bot.")
+# MongoDB connection configuration
+mongo_client = MongoClient('mongodb+srv://tylerlian2021:CytaQbiGsjwFwe8k@bfgsafespace.tnob2zh.mongodb.net/')
+db = mongo_client['your_database_name']  # Replace 'your_database_name' with your desired database name
 
-# Handler for text messages
-def handle_message(update: Update, context):
+# Telegram bot configuration
+bot_token = '6274496403:AAGehEhKyVAGNOQ8c75y8Y5DE-Muu9x1w50'  # Replace 'your_bot_token' with the actual bot token
+bot = Bot(token=bot_token)
+
+# Start command handler
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! Welcome to the bot.')
+
+start_handler = CommandHandler('start', start)
+
+# Command to store data in MongoDB
+def store_data(update, context):
+    user_id = update.effective_user.id
     message_text = update.message.text
-    response_text = f"You said: {message_text}"
-    context.bot.send_message(chat_id=update.message.chat_id, text=response_text)
 
-def main():
-    # Create the Telegram Bot updater
-    updater = Updater("6274496403:AAGehEhKyVAGNOQ8c75y8Y5DE-Muu9x1w50", use_context=True)
+    # Store data in MongoDB
+    collection = db['messages']
+    collection.insert_one({'user_id': user_id, 'message': message_text})
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Data stored successfully.')
 
-    # Register command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
+store_data_handler = CommandHandler('store', store_data)
 
-    # Register message handler
-    dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+# Command to retrieve data from MongoDB
+def retrieve_data(update, context):
+    user_id = update.effective_user.id
 
-    # Start the Bot
-    updater.start_polling()
-    updater.idle()
+    # Retrieve data from MongoDB
+    collection = db['messages']
+    result = collection.find({'user_id': user_id})
 
-if __name__ == '__main__':
-    main()
+    messages = [doc['message'] for doc in result]
+    response = '\n'.join(messages)
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+retrieve_data_handler = CommandHandler('retrieve', retrieve_data)
+
+# Create the Telegram bot instance
+updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
+# Add the handlers to the dispatcher
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(store_data_handler)
+dispatcher.add_handler(retrieve_data_handler)
+
+# Start the bot
+updater.start_polling()
--- flaskApplication.py
+++ flaskApplication.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# MongoDB connection configuration
+app.config['MONGO_URI'] = 'mongodb+srv://tylerlian2021:CytaQbiGsjwFwe8k@bfgsafespace.tnob2zh.mongodb.net/'
+mongo_client = MongoClient(app.config['MONGO_URI'])
+db = mongo_client['mydatabase']  # Replace 'mydatabase' with your desired database name
+
+# Sample route to retrieve data from MongoDB
+@app.route('/users', methods=['GET'])
+def get_users():
+    users_collection = db.users  # Replace "users" with your desired collection name
+    users = list(users_collection.find())
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
