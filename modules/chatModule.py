36abd0c98d89338fa8693142ea3433329e318cd6
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-06-28 20:22:53 +0800

    Happiness algo

--- modules/chatModule.py
+++ modules/chatModule.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import os
 import random
-
 load_dotenv()
 
 # MongoDB connection configuration
@@ -39,39 +38,78 @@ def match_partner(update: Update, context):
         context.bot.send_message(chat_id=partnerid, text="Conversation cancelled. Please use /begin for a new partner!")
 
     # Matching the user with the next available partner
-    data = collection.find()
+    data = collection.aggregate([
+        {
+            '$addFields': {
+                'happiness_numeric': {
+                    '$cond': {
+                        'if': { '$eq': ['$happiness', 'NA'] },
+                        'then': -1,  # Assign a lower value for 'NA'
+                        'else': { '$toInt': '$happiness' }  # Convert other values to numeric
+                    }
+                }
+            }
+        },
+        { '$sort': { 'happiness_numeric': -1 } }  # Sort by 'happiness_numeric' field in descending order
+    ])
     # Getting most updated user details
     user = collection.find_one({'userid': userid})
+    userHappiness = int(user['happiness'])
     pastPartners = user['pastPartners']
-    tempPartners = []
-    matched = False
+    secondPartners = []
+    firstPartners = []
+    NAPartners = []
     finalPartner = None
     for item in data:
         # logic to determine the suitable happiness level of chat partner
-        # happy match with sad (Comment out temporarily)
-        # condition = int(item['happiness']) <= 5 if int(user['happiness']) > 5 else int(item['happiness']) > 5
         # Filtering out those with partners and own user ID
-        if item['partnerid'] is None and item['userid'] != user['userid'] and item['userid'] not in user['reportedUsers'] and item['available'] == True:
+        if (item['partnerid'] is None) and (item['userid'] != user['userid']) and (item['userid'] not in user['reportedUsers']) and (item['available'] == True):
             itemPartner = item['userid']
-            # If time difference less than 1 day
+            # If time difference less than 1 day (Lower priority group)
             if str(itemPartner) in pastPartners and datetime.now() - pastPartners[str(itemPartner)] <= timedelta(days=1):
-                tempPartners.append(itemPartner)
+                secondPartners.append(itemPartner)
+            elif item['happiness_numeric'] == -1: # NA Happiness group
+                NAPartners.append(itemPartner)
             else:
-                collection.update_one({'userid': userid}, {'$set': {'partnerid': itemPartner}})
-                collection.update_one({'userid': itemPartner}, {'$set': {'partnerid': userid}})
-                matched = True
-                finalPartner = itemPartner
-                break
-    if not matched:
-        print(tempPartners)
-        if tempPartners == []:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="No users available at the moment. Please try again later!")
-            collection.update_one({'userid': userid}, {'$set': {'available': False}})
-            return ConversationHandler.END
-        randomPartner = random.choice(tempPartners)
+                firstPartners.append((itemPartner, item['happiness_numeric']))
+
+    # If there are any possible users in the priority group, use them
+    if len(firstPartners) != 0:
+        print("First priority group is:" + firstPartners)
+        # first partners are already sorted from highest to lowest
+        max_difference = float('-inf')
+        max_difference_user = None
+        for partnerid, partnerHappiness in firstPartners:
+            difference = abs(partnerHappiness - userHappiness)
+            if difference > max_difference:
+                max_difference = difference
+                max_difference_user = partnerid
+        collection.update_one({'userid': userid}, {'$set': {'partnerid': max_difference_user}})
+        collection.update_one({'userid': max_difference_user}, {'$set': {'partnerid': userid}})
+        finalPartner = max_difference_user
+
+    # If there are any possible users in the lower priority group, use them
+    if len(firstPartners) == 0 and len(secondPartners) != 0:
+        print("Second priority group is:" + secondPartners)
+        randomPartner = random.choice(secondPartners)
         collection.update_one({'userid': userid}, {'$set': {'partnerid': randomPartner}})
         collection.update_one({'userid': randomPartner}, {'$set': {'partnerid': userid}})
-        finalPartner = randomPartner    
+        finalPartner = randomPartner 
+
+    # If there are any possible users in the NA priority group, use them
+    if len(firstPartners) == 0 and len(secondPartners) == 0 and len(NAPartners) != 0: 
+        print("NA priority group is:" + NAPartners)
+        randomPartner = random.choice(NAPartners)
+        collection.update_one({'userid': userid}, {'$set': {'partnerid': randomPartner}})
+        collection.update_one({'userid': randomPartner}, {'$set': {'partnerid': userid}})
+        finalPartner = randomPartner 
+
+    # No possible users to match with at all
+    if len(firstPartners) == 0 and len(secondPartners) == 0 and len(NAPartners) == 0: 
+        context.bot.send_message(chat_id=update.effective_chat.id, text="No users available at the moment. Please try again later!")
+        # collection.update_one({'userid': userid}, {'$set': {'available': False}})
+        return ConversationHandler.END   
+    
     context.bot.send_message(chat_id=update.effective_chat.id, text="Matched! Say hi to your partner! If at any point your partner does not make you feel comfortable, you can report them by using /report!")
     context.bot.send_message(chat_id=finalPartner, text="Matched! Say hi to your partner! If at any point your partner does not make you feel comfortable, you can report them by using /report!")
     return
